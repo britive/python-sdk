18427e0e123b6441968f146103d87d45fd61e802
Thomas Rawley <thomas.rawley@britive.com>
2022-07-12 14:29:45 -0400

    profile approval

--- src/britive/exceptions.py
+++ src/britive/exceptions.py
@@ -87,6 +87,10 @@ class TenantNotEnabledForProfilesVersion2(Exception):
     pass
 
 
+class ApprovalWorkflowRejected(Exception):
+    pass
+
+
 # from https://docs.britive.com/docs/restapi-status-codes
 allowed_exceptions = {
     400: InvalidRequest,
