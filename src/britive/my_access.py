8828009f01b5d367b2e9553b9646782d1cee6a90
theborch <theborch@gmail.com>
2024-09-09 15:37:48 -0500

    fix:allow approver comments

--- src/britive/my_access.py
+++ src/britive/my_access.py
@@ -273,29 +273,33 @@ class MyAccess:
 
         return self.britive.delete(url)
 
-    def approve_request(self, request_id: str) -> None:
+    def approve_request(self, request_id: str, comments: str = None) -> None:
         """
         Approves a request.
 
         :param request_id: The ID of the request.
+        :param comments: Approver comments.
         :return: None.
         """
 
         params = {'approveRequest': 'yes'}
+        data = {'approverComment': comments or ""}
 
-        return self.britive.patch(f'{self.britive.base_url}/v1/approvals/{request_id}', params=params)
+        return self.britive.patch(f'{self.britive.base_url}/v1/approvals/{request_id}', params=params, json=data)
 
-    def reject_request(self, request_id: str):
+    def reject_request(self, request_id: str, comments: str = None) -> None:
         """
         Rejects a request.
 
         :param request_id: The ID of the request.
+        :param comments: Approver comments.
         :return: None.
         """
 
         params = {'approveRequest': 'no'}
+        data = {'approverComment': comments or ""}
 
-        return self.britive.patch(f'{self.britive.base_url}/v1/approvals/{request_id}', params=params)
+        return self.britive.patch(f'{self.britive.base_url}/v1/approvals/{request_id}', params=params, json=data)
 
     def list_approvals(self) -> dict:
         """
