cb9f97ca28c93bf44db7954f3b3283d7a3c2acf3
theborch <theborch@gmail.com>
2025-03-06 11:39:33 -0600

    fix:add missing fields param

--- src/britive/access_broker/resources/types.py
+++ src/britive/access_broker/resources/types.py
@@ -12,18 +12,31 @@ class Types:
 
         return self.britive.get(self.base_url)['data']
 
-    def create(self, name, description='Default resource type description') -> dict:
+    def create(self, name: str, description: str = '', fields: list = None) -> dict:
         """
         Create a new resource type.
 
         :param name: Name of the resource type.
         :param description: Description of the resource type.
+        :param fields: List of Resource Type fields.
+            Example: [
+                {
+                    'name': 'string',
+                    'paramType': 'string'|'multiline'|'password',
+                    'isMandatory': True|False
+                },
+                ...
+            ]
         :return: Created resource type.
         """
         params = {
             'name': name,
             'description': description,
         }
+
+        if fields:
+            params['parameters'] = fields
+
         return self.britive.post(self.base_url, json=params)
 
     def get(self, resource_type_id) -> dict:
@@ -35,15 +48,29 @@ class Types:
         """
         return self.britive.get(f'{self.base_url}/{resource_type_id}')
 
-    def update(self, resource_type_id, description=None) -> dict:
+    def update(self, resource_type_id: str, description: str = None, fields: list = None) -> dict:
         """
         Update a resource type.
 
         :param resource_type_id: ID of the resource type.
         :param description: Description of the resource type.
+        :param fields: List of Resource Type fields.
+            Example: [
+                {
+                    'name': 'string',
+                    'paramType': 'string'|'multiline'|'password',
+                    'isMandatory': True|False
+                },
+                ...
+            ]
         :return: Updated resource type.
         """
-        params = {'description': description}
+        params = {}
+
+        if description:
+            params['description'] = description
+        if fields:
+            params['parameters'] = fields
 
         return self.britive.put(f'{self.base_url}/{resource_type_id}', json=params)
 
