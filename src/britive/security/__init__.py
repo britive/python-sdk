b2ac54b9c6a316556f811ccdbd9a37c5055b2206
Sam <sam@wanfamily.org>
2025-07-07 14:10:40 -0700

    feat: active session support

--- src/britive/security/__init__.py
+++ src/britive/security/__init__.py
@@ -2,6 +2,7 @@ from .api_tokens import ApiTokens
 from .policies import SecurityPolicies
 from .saml import Saml
 from .step_up import StepUpAuth
+from .active_sessions import ActiveSessions
 
 
 class Security:
@@ -10,3 +11,4 @@ class Security:
         self.saml = Saml(britive)
         self.security_policies = SecurityPolicies(britive)
         self.step_up_auth = StepUpAuth(britive)
+        self.active_sessions = ActiveSessions(britive)
---
+++ src/britive/security/active_sessions.py
@@ -0,0 +1,45 @@
+class ActiveSessions:
+    def __init__(self, britive) -> None:
+        self.britive = britive
+        self.base_url = f'{self.britive.base_url}/paps/sessions'
+    
+    def list_users(self) -> list:
+        """
+        Retrieve a list of the users with active sessions
+        
+        :return: list of users
+        """
+
+        return self.britive.get(self.base_url)
+
+    def list(self, user_id: str) -> list:
+        """
+        Retrieve a list of the active sessions of a user
+        
+        :param user_id: The target user
+        :return: The list of the user's active sessions
+        """
+
+        return self.britive.get(f'{self.base_url}/{user_id}')
+    
+    def delete(self, user_id: str, profile_id: str) -> None:
+        """
+        Kills a user's session.
+        
+        :param user_id: The target user
+        :param profile_id: The target profile that has been checked out
+        :return: None
+        """
+
+        sessions = self.britive.get(f'{self.base_url}/{user_id}')
+        target = None
+
+        for item in sessions:
+            if item['papId'] == profile_id:
+                target = item['transactionId']
+                break
+        
+        if not target:
+            return
+
+        return self.britive.delete(f'{self.base_url}/{target}') 
