6891e98abff646bcce065da8fb242903c369825e
theborch <theborch@gmail.com>
2025-07-08 14:36:38 -0500

    refactor(active_sessions):missing resources, checkin_all, and searchText

--- src/britive/security/active_sessions.py
+++ src/britive/security/active_sessions.py
@@ -1,42 +1,61 @@
 class ActiveSessions:
     def __init__(self, britive) -> None:
         self.britive = britive
-        self.base_url = f'{self.britive.base_url}/paps/sessions'
+        self.base_urls = {
+            'applications': f'{self.britive.base_url}/paps/sessions',
+            'resources': f'{self.britive.base_url}/resource-manager/sessions',
+        }
 
-    def list_users(self) -> list:
+    def list_users(self, search_text: str = None) -> list:
         """
-        Retrieve a list of the users with active sessions
+        Retrieve a list of users with active session(s), i.e. checked out profiles.
 
-        :return: list of users
-        """
-
-        return self.britive.get(self.base_url)
-
-    def list_users_sessions(self, user_id: str) -> list:
-        """
-        Retrieve a list of the active sessions of a user
-
-        :param user_id: The target user
-        :return: The list of the user's active sessions
+        :return: List of users with active session(s).
         """
 
-        return self.britive.get(f'{self.base_url}/{user_id}')
+        params = {}
 
-    def checkin(self, user_id: str, profile_ids: list = None) -> None:
+        if search_text:
+            params['searchText'] = search_text
+
+        return self.britive.get(self.base_urls['applications'], params=params)
+
+    def list_user_sessions(self, user_id: str) -> dict:
         """
-        Checks in a user's session.
+        Retrieve the active sessions (checked out profiles) of a given user.
 
-        :param user_id: The target user
-        :param profile_id: The target profile that has been checked out
+        :param user_id: The target user's ID.
+        :return: Dict of the user's active Application and Resources sessions.
+        """
+
+        return {
+            'applications': self.britive.get(f'{self.base_urls["applications"]}/{user_id}'),
+            'resources': self.britive.get(f'{self.base_urls["resources"]}/{user_id}'),
+        }
+
+    def checkin(self, user_id: str, profile_ids: list) -> None:
+        """
+        Checkin one or more active profile sessions for a given user.
+
+        :param user_id: The target user's ID.
+        :param profile_ids: List of target profile ID(s) to checkin.
+        :return: None
+        """
+        for transaction_id, profile_type in {
+            session['transactionId']: profile_type
+            for profile_type, sessions in self.list_user_sessions(user_id).items()
+            for session in sessions
+            if session.get('papId', session.get('profileId')) in profile_ids
+        }.items():
+            self.britive.delete(f'{self.base_urls[profile_type]}/{transaction_id}')
+
+    def checkin_all(self, user_id: str) -> None:
+        """
+        Checkin all active profiles sessions for a given user.
+
+        :param user_id: The target user's ID
         :return: None
         """
 
-        if not profile_ids:
-            print('No profiles checked in due to empty profile_ids')
-        else:
-            sessions = self.britive.get(f'{self.base_url}/{user_id}')
-            target = None
-            for item in sessions:
-                if item['papId'] in profile_ids:
-                    target = item['transactionId']
-                    self.britive.delete(f'{self.base_url}/{target}')
+        self.britive.delete(f'{self.base_urls["applications"]}/user/{user_id}')
+        self.britive.delete(f'{self.base_urls["resources"]}/checkin-all/{user_id}')
