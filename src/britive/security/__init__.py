15981e2418850ebede21884f6391250bae252a34
Sam <sam@wanfamily.org>
2025-07-07 17:27:35 -0700

    feat and chore: linting and delete all users

--- src/britive/security/__init__.py
+++ src/britive/security/__init__.py
@@ -1,14 +1,14 @@
+from .active_sessions import ActiveSessions
 from .api_tokens import ApiTokens
 from .policies import SecurityPolicies
 from .saml import Saml
 from .step_up import StepUpAuth
-from .active_sessions import ActiveSessions
 
 
 class Security:
     def __init__(self, britive) -> None:
+        self.active_sessions = ActiveSessions(britive)
         self.api_tokens = ApiTokens(britive)
         self.saml = Saml(britive)
         self.security_policies = SecurityPolicies(britive)
         self.step_up_auth = StepUpAuth(britive)
-        self.active_sessions = ActiveSessions(britive)
--- src/britive/security/active_sessions.py
+++ src/britive/security/active_sessions.py
@@ -2,11 +2,11 @@ class ActiveSessions:
     def __init__(self, britive) -> None:
         self.britive = britive
         self.base_url = f'{self.britive.base_url}/paps/sessions'
-    
+
     def list_users(self) -> list:
         """
         Retrieve a list of the users with active sessions
-        
+
         :return: list of users
         """
 
@@ -15,17 +15,17 @@ class ActiveSessions:
     def list(self, user_id: str) -> list:
         """
         Retrieve a list of the active sessions of a user
-        
+
         :param user_id: The target user
         :return: The list of the user's active sessions
         """
 
         return self.britive.get(f'{self.base_url}/{user_id}')
-    
+
     def delete(self, user_id: str, profile_id: str) -> None:
         """
         Kills a user's session.
-        
+
         :param user_id: The target user
         :param profile_id: The target profile that has been checked out
         :return: None
@@ -37,9 +37,24 @@ class ActiveSessions:
         for item in sessions:
             if item['papId'] == profile_id:
                 target = item['transactionId']
-                break
-        
-        if not target:
-            return
+                self.britive.delete(f'{self.base_url}/{target}')
 
-        return self.britive.delete(f'{self.base_url}/{target}') 
+    def delete_all(self) -> None:
+        """
+        Kills all users' sessions
+
+        :return: None
+        """
+
+        users = self.britive.get(self.base_url)
+        total_sessions = []
+
+        for user in users:
+            user_id = user['userId']
+            sessions = self.britive.get(f'{self.base_url}/{user_id}')
+            for session in sessions:
+                total_sessions.append(session)
+
+        for item in total_sessions:
+            target = item['transactionId']
+            self.britive.delete(f'{self.base_url}/{target}')
