46e4e3ba69d76489f2838dec82adcbadea11f866
theborch <theborch@gmail.com>
2025-10-24 13:35:08 -0500

    test:update naming convention

--- tests/000-global_settings-02-notification_mediums.py
+++ tests/000-global_settings-02-notification_mediums.py
@@ -3,7 +3,7 @@ from .cache import *  # will also import some globals like `britive`
 
 def test_create(cached_notification_medium):
     assert isinstance(cached_notification_medium, dict)
-    assert 'pytest-nm-' in cached_notification_medium['name']
+    assert 'pysdktest-nm-' in cached_notification_medium['name']
 
 
 def test_list():
@@ -20,7 +20,7 @@ def test_get(cached_notification_medium):
 
 def test_update(cached_notification_medium):
     r = str(random.randint(0, 999))
-    new_name = f'{cached_notification_medium["name"]}-{r}'
+    new_name = f'{r}-{cached_notification_medium["name"]}'[:30]
     britive.global_settings.notification_mediums.update(cached_notification_medium['id'], parameters={'name': new_name})
     response = britive.global_settings.notification_mediums.get(cached_notification_medium['id'])
     assert response['name'] == new_name
